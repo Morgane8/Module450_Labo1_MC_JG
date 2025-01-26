948318d659403445544c494e13210d7a359276c1
Morgane8 <Morgane.cachin@bluewin.ch>
2025-01-26 12:39:53 +0100

    TDD iteration 1 : R (user story 1 : acceptance criteria 1)

--- tests/test_checkers.py
+++ tests/test_checkers.py
@@ -1,1 +1,17 @@
 # This is the file in which we will write our tests for the checkers module.
+
+from verify.checkers import Checkers
+import pytest
+
+# TDD: iteration 1
+# arrange
+@pytest.mark.parametrize("email_address, expected_value", [
+    ("john@doe.com", True),
+    ("@doe.com", False),
+    ("john.doe@mail.com", True),
+])
+def test_name_before_at_sign(email_address, expected_value):
+    # act
+    returned_value = Checkers.check_if_name_before_at_sign(email_address)
+    # assert
+    assert returned_value == expected_value
--- verify/checkers.py
+++ verify/checkers.py
@@ -1,1 +1,6 @@
 # Here is the class in which we'll have all checking methods
+
+class Checkers:
+    @staticmethod
+    def check_if_name_before_at_sign(email_address):
+        pass

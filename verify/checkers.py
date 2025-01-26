92ccaa3bef21d30ab817483f59fb171c191de6af
Morgane8 <Morgane.cachin@bluewin.ch>
2025-01-26 12:46:22 +0100

    TDD iteration 1 : G (check for local part before the at sign)

--- verify/checkers.py
+++ verify/checkers.py
@@ -1,6 +1,12 @@
 # Here is the class in which we'll have all checking methods
 
+import re
+
 class Checkers:
     @staticmethod
     def check_if_name_before_at_sign(email_address):
-        pass
+        local_regex = re.compile(r"^[a-zA-Z0-9._%+-]+@")
+        if re.match(local_regex, email_address):
+            return True
+        else:
+            return False
